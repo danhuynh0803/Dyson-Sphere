91ed6acd120e98c3cf1d4ca6e230628d0c6568c9
danhuynh0803 <danhuynh0803@gmail.com>
2020-02-01 14:06:48 -0500

    Basic meteor spawner

---
+++ Assets/Scripts/House.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class House : MonoBehaviour
+{
+    public GameObject[] houseProgressionModels;
+    public int[] tierRequirement;
+
+    private bool hasSwappedTier;
+    private int tier = 0;
+
+    private int currScore = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check that a player has enough fragments to advance a tier
+        if (!hasSwappedTier)
+        {
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Absorb fragments
+        if (other.gameObject.GetComponent<Fragment>() != null)
+        {
+            currScore++;
+            Destroy(other.gameObject);
+        }
+    }
+}
