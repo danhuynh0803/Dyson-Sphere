9cd687643a74af20c4cb384e664176e408858021
ipsofacto537 <60493150+ipsofacto537@users.noreply.github.com>
2020-02-01 17:35:19 -0500

    fixed eyes
    sounds

---
+++ Assets/Bob.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+    public float amplitude;
+    public float frequency;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    void Update()
+    {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
