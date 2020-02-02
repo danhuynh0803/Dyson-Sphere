47ab69617d8d174138f4aeb6ddccf16b94d82bf5
ipsofacto537 <60493150+ipsofacto537@users.noreply.github.com>
2020-02-02 00:18:53 -0500

    Worked on winning condition

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -9,6 +9,16 @@ public class House : MonoBehaviour
     public Material p1Color, p2Color;
 
     private int currScore = 0;
+    private int redPoint = 0;
+    private int bluePoint = 0;
+
+    public GameObject cameraOne;
+    public GameObject cameraTwo;
+    public GameObject cameraThree;
+    public GameObject playerRed;
+    public GameObject playerBlue;
+    public GameObject redWins;
+    public GameObject blueWins;
 
     void Start()
     {
@@ -18,12 +28,12 @@ public class House : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (currScore >= maxScore)
+        /*if (currScore >= maxScore)
         {
             // Setting this to true will trigger win scene
             // TODO maybe make a win panel instead
             GameController.instance.isGameOver = true;
-        }
+        }*/
     }
 
     void OnTriggerEnter(Collider other)
@@ -39,16 +49,40 @@ public class House : MonoBehaviour
             if (controllingPlayer == Player.P1)
             {
                 houseProgressionModels[currScore].GetComponent<Renderer>().material = p1Color;
+                redPoint++;
             }
             else if (controllingPlayer == Player.P2)
             {
                 houseProgressionModels[currScore].GetComponent<Renderer>().material = p2Color;
+                bluePoint++;
             }
 
             Destroy(other.gameObject);
             houseProgressionModels[currScore].SetActive(true);
 
             currScore++;
+
+            if (currScore >= maxScore)
+            {
+                EndGame();
+            }
         }
+    }
+        void EndGame()
+    {
+        cameraOne.SetActive(false);
+        cameraTwo.SetActive(false);
+        cameraThree.SetActive(true);
+        playerRed.SetActive(false);
+        playerBlue.SetActive(false);
+        if (redPoint > bluePoint)
+        {
+            redWins.SetActive(true);
+        }
+        else
+        {
+            blueWins.SetActive(true);
+        }
+
     }
 }
